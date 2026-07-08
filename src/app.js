6bc3653e2f8018df3e9a44d31c2857073ab14027
Hamad Ahmad <hamadatusa@gmail.com>
2026-07-08 20:10:05 +0500

    Set up Neon Postgres w/ Drizzle

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 // this file is all about setting up the express application with the right middleware
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from Acquisitions!");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from Acquisitions!');
 });
 
 export default app;
